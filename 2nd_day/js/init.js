834399cca4bceeca3aad5c463eab75d4e75b891d
gitlec4 <38457237+gitlec4@users.noreply.github.com>
2018-04-17 18:22:41 +0900

    2일차

--- 2nd_day/js/init.js
+++ 2nd_day/js/init.js
@@ -1,12 +1,19 @@
 jQuery(function($){
+    $('#mainSlider').bxSlider({
+        //speed:1\
+        mode:'fade'
+    });
+    
     var idx=0;
-    $('.control .btn-prev').on('click',function(){
+    $('.control .btn-prev').on('click',function(e){
+        e.preventDefault();
         if(idx<1){idx=4}
         else{idx--;}
         
         $('.main-slider .slide').eq(idx).addClass('on').siblings().removeClass('on');
     });
-    $('.control .btn-next').on('click',function(){
+    $('.control .btn-next').on('click',function(e){
+        e.preventDefault();
         if(idx>3){idx=0}
         else{idx++;}
         
